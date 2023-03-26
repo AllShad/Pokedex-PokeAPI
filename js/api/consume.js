85e4684f9099902da85f07f724fde359d024eb09
AllShad <gabrielmoreira972@gmail.com>
2023-03-26 18:05:22 -0300

    Finalização da estilização do card, adição das cores dos tipos e aleatóriedade na busca dos pokemons

--- js/api/consume.js
+++ js/api/consume.js
@@ -5,57 +5,13 @@ const closeCard = document.querySelector('#close');
 const loadingCard = document.querySelector('#loading-card');
 const cardSection = document.querySelector('#card > section')
 
-function getPokemon(urlPoke, repeat, endReapts){
+function getPokemon(urlPoke){
     fetch(urlPoke).then(function(response){
         response.json().then(function(data){
-            showData(data, repeat, endReapts);
+            showData(data);
         })
     })
 
-}
-
-async function getPokemons(){
-    fetch(url).then(function(response){
-        response.json().then(function(data){
-            loading.classList.add('hide')
-
-            for(let i = 0; i < data.results.length; i++){
-                getPokemon(data.results[i].url, i, data.results.length) 
-            } 
-        })
-    })
-}
-
-function showData(data ,repeat, endReapts){
-    const image = document.createElement('img');
-    const h1 = document.createElement('h1')
-    const div = document.createElement('div');
-
-    image.src = data.sprites.front_default;
-    h1.textContent = data.name.toUpperCase();
-
-    div.appendChild(h1);
-    div.appendChild(image);
-
-    div.classList.add('picture');
-
-    image.classList.add('pokemon-picture');
-    image.classList.add(h1.textContent);
-
-    h1.classList.add('pokemon-picture');
-    h1.classList.add(h1.textContent);
-
-    section.appendChild(div); 
-    
-    if(repeat == endReapts - 1){
-        const pokemon = document.querySelector('.screan').children
-
-        for(let poke of pokemon){
-            poke.addEventListener('click', function(){
-                getPokeFillCard(poke.textContent);
-            }) 
-        }
-    }
 }
 
 function getPokeFillCard(name){
@@ -67,6 +23,113 @@ function getPokeFillCard(name){
     })
 }
 
+async function getPokemons(){
+    loading.classList.add('hide')
+    for(let i = 0; i < 20; i++){
+        const getPokeRandom = url + '/' + Math.floor(Math.random() * 1000)
+        getPokemon(getPokeRandom);
+    }
+}
+
+function colorType(type, div){    
+    switch(type){
+        case 'normal':
+            div.style.backgroundColor = '#B7B7A9'
+            break;
+        case 'fighting':
+            div.style.backgroundColor = '#C56E60'
+            break;
+        case 'flying':
+            div.style.backgroundColor = '#9AA9FF'
+            break;
+        case 'poison':
+            div.style.backgroundColor = '#B76FA9'
+            break;
+        case 'ground':
+            div.style.backgroundColor = '#E2C56F'
+            break;
+        case 'rock':
+            div.style.backgroundColor = '#C5B77D'
+            break;
+        case 'bug':
+            div.style.backgroundColor = '#B7C544'
+            break;
+        case 'ghost':
+            div.style.backgroundColor = '#7D7DC5'
+            break;
+        case 'steel':
+            div.style.backgroundColor = '#B7B7C5'
+            break;
+        case 'fire':
+            div.style.backgroundColor = '#FF6144'
+            break;
+        case 'water':
+            div.style.backgroundColor = '#52A9FF'
+            break;
+        case 'grass':
+            div.style.backgroundColor = '#8CD46F'
+            break;
+        case 'electric':
+            div.style.backgroundColor = '#FFD351'
+            break;
+        case 'psychic':
+            div.style.backgroundColor = '#FF6FA9'
+            break;
+        case 'ice':
+            div.style.backgroundColor = '#7CD3FF'
+            break;
+        case 'dragon':
+            div.style.backgroundColor = '#8C7DF1'
+            break;
+        case 'dark':
+            div.style.backgroundColor = '#8B6E60'
+            break;
+        case 'fairy':
+            div.style.backgroundColor = '#F1A8F1'
+            break;
+        case 'unknown':
+            div.style.backgroundColor = '#98C4B8'
+            break;
+        case 'shadow':
+            div.style.backgroundColor = '#000000'
+            break;
+    }
+}
+
+function showData(data){
+    const image = document.createElement('img');
+    const h1 = document.createElement('h1')
+    const div = document.createElement('div');
+
+    image.src = data.sprites.front_default;
+    h1.textContent = data.name.toUpperCase();
+    const type = data.types[0].type.name;
+
+    div.appendChild(h1);
+    div.appendChild(image);
+
+    div.classList.add('picture');
+    div.classList.add(type)
+
+    image.classList.add('pokemon-picture');
+    image.classList.add(h1.textContent);
+
+    h1.classList.add('pokemon-name');
+    h1.classList.add(h1.textContent);
+
+    section.appendChild(div); 
+
+    colorType(type, div)
+    
+    const pokemon = document.querySelector('.screan').children
+
+    for(let poke of pokemon){
+        poke.addEventListener('click', function(){
+            getPokeFillCard(poke.textContent);
+        }) 
+    }
+}
+
 function fillInCard(data){
     makeCard();
     loadingCard.classList.add('hide')
@@ -76,6 +139,9 @@ function fillInCard(data){
 
     pokeName.textContent = data.name.toUpperCase();
     pokeImage.src = data.sprites.front_default;
+
+    const div = document.querySelector('#card');
+    colorType(data.types[0].type.name, div)
 
     fillAtacks(data);
 }
