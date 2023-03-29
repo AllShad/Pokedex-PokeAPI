93d21511998635adf7ba029b52abdc009f6b19de
AllShad <gabrielmoreira972@gmail.com>
2023-03-29 06:49:12 -0300

    criação das páginas extras e construção da página de tipos

--- js/functions.js
+++
@@ -1,4 +0,0 @@
-export default function testeImport(){
-    console.log('teste')
-}
-
---
+++ js/type-collection.js
@@ -0,0 +1,26 @@
+const url = 'https://pokeapi.co/api/v2/type';
+const section = document.getElementById('content');
+
+function getType(){
+    fetch(url).then(function(response){
+        response.json().then(function(data){
+            console.log(data)
+        })
+    })
+}
+
+function addOnScrean(data){
+    const div = document.createElement('div');
+    const h1 = document.createElement('h1');
+    const img = document.createElement('img');
+
+    div.classList.add('picture');
+    img.classList.add('pokemon-picture');
+
+    div.appendChild(h1);
+    div.appendChild(img);
+
+    section.appendChild(div);
+}
+
+addOnScrean()
