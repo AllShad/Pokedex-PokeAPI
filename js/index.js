f900f35cb22a64c60f92073b480f711ca4ffc2a1
AllShad <gabrielmoreira972@gmail.com>
2023-04-16 20:22:53 -0300

    Finalização da barra de pesquisa

--- js/index.js
+++ js/index.js
@@ -35,17 +35,54 @@ function addPart(pokemon){
     const image = document.createElement('img');
     const name = document.createElement('h1');
     
-    name.textContent = pokemon.name;
+    name.textContent = pokemon.name.toUpperCase();
     image.src = pokemon.sprites.front_default;
 
     infos[0].appendChild(name);
     infos[0].appendChild(image);
 
-    // const atacks = pokemon.
+    const types = pokemon.types
+    const typesList = document.createElement('ul');
+    infos[0].appendChild(typesList);
+
+    for(let type of types){
+        const li = document.createElement('li');
+        const h1 = document.createElement('h1')
+        h1.textContent = type.type.name;
+        li.appendChild(h1);
+        typesList.appendChild(li); 
+    }
+
+    const atacks = pokemon.abilities;
+
+    const atackList = document.createElement('ul')
+    infos[1].appendChild(atackList)
+
+    for(let atack of atacks){
+        const li = document.createElement('li');
+        const h1 = document.createElement('h1')
+        h1.textContent = atack.ability.name;
+        li.appendChild(h1);
+        atackList.appendChild(li);
+    }
+
+    const status = pokemon.stats;
+    const statusList = document.createElement('ul')
+    infos[2].appendChild(statusList);
+
+    for(let stats of status){
+        const li = document.createElement('li');
+        const h1 = document.createElement('h1')
+        h1.textContent = `${stats.stat.name}: ${stats.base_stat}`
+        li.appendChild(h1);
+        statusList.appendChild(li);
+    }
+
 }
 
 buttonSearch.addEventListener('click', getPokeByName);
 image.addEventListener('click', () => {
     changeStyle.href = './css/index.css';
     section.classList.add('hide');
+    window.location.href = '../index.html'
 })
